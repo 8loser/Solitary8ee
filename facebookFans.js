9e711578a1376c551ccf161b49f3f93538b01ffe
8loser@gmail.com <8loser@gmail.com>
2021-02-13 01:06:55 +0800

    增加載入 cookie功能

--- facebookFans.js
+++ facebookFans.js
@@ -1,16 +1,26 @@
 const puppeteer = require('puppeteer');
 const { auth } = require('./config');
 const { fan } = require('./config');
+const fs = require('fs');
+
 
 (async () => {
   const baseURL = 'https://www.facebook.com/'
   // const browser = await puppeteer.launch({ ignoreDefaultArgs: ["--enable-automation"] });
   const browser = await puppeteer.launch({
     headless: false,
-    // slowMo: 0,
+    args: ['--disable-notifications'],
     // args: ['--no-sandbox', '--disable-setuid-sandbox']
   });
   const page = await browser.newPage();
+  // TODO 判斷有沒有 cookies 檔案或者檔案是否為空，如果有資料就載入，同時不跳登入畫面
+  // Load cookie
+  const cookiesString = await fs.readFileSync('./cookies.json', 'utf8');
+  const cookies = JSON.parse(cookiesString);
+  await page.setCookie(...cookies);
+
+  // 是否有下一筆
+  let hasNext = true
 
   const detected = async (interceptedResponse) => {
     if (interceptedResponse.url() === 'https://www.facebook.com/api/graphql/'
@@ -27,8 +37,12 @@ const { fan } = require('./config');
               console.log(id, name, url)
             })
           }
+          // 判斷是否有下一筆
+          if (respJSON.data.node.top_fans.page_info.has_next_page === true) {
+            hasNext = true
+          }
         } catch (error) {
-          // nothing
+          hasNext = false
         }
       }
     }
@@ -47,8 +61,16 @@ const { fan } = require('./config');
     await page.type(passwordField, auth.pass);
     await page.click('button[name=login]');
     await page.waitForNavigation();
-  }
 
+    // Save cookie
+    // const cookies = await page.cookies();
+    // await fs.writeFile('cookies.json', JSON.stringify(cookies, null, 2), function (err) {
+    //   if (err)
+    //     console.log(err);
+    //   else
+    //     console.log('Write operation complete.');
+    // });
+  }
 
   /**
    * 抓資料
@@ -56,13 +78,24 @@ const { fan } = require('./config');
   let grab = async () => {
     await page.goto(`${baseURL}${fan.account}/community/?ref=page_internal`);
 
-    await page.waitForSelector('div[role = button] > span[dir = auto]');
-    await page.click('div[role = button] > span[dir = auto]');
+    await page.waitForSelector('div[role=button]>span[dir=auto]');
+    await page.click('div[role=button]>span[dir=auto]');
+
+    // 等待資料載入
+    await page.waitForTimeout(1500)
+    const pageView = page.viewport()
+    await page.mouse.click(
+      pageView.width / 2,
+      pageView.height / 2
+    );
+    while (hasNext) {
+      await page.mouse.wheel({ deltaY: 6 })
+    }
   }
 
-  await login()
+  // await login()
   page.on('response', detected);
   await grab()
 
-  // await browser.close();
+  await browser.close();
 })();
